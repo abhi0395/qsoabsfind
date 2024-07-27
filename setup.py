56ceb537b3fb6477a5355bee7c80e4f15a8a3806
a_anand <abhijeetanand2011@gmail.com>
2024-07-26 17:01:42 -0700

    tests and README added

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='qsoabsfind',
     version='0.1.0',
-    description='A package to find QSO absorbers using convolution methods',
+    description='A module to search for MgII/CIV absorption features in QSO spectra using convolution based adaptive S/N approach',
     packages=find_packages(),
     install_requires=[
         'numpy',
         'astropy',
         'scipy',
         'fitsio',
+        'numba',
     ],
     entry_points={
         'console_scripts': [
@@ -19,10 +20,8 @@ setup(
     package_data={
         'qsoabsfind': ['*.py'],
     },
-
     author='Abhijeet Anand',
     author_email='AbhijeetAnand@lbl.gov',
-    description='A module to search for MgII/CIV absorption features in QSO spectra.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/abhi0395/qsoabsfind',
