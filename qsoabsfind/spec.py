588479e5b0edc8bff92d70928f1178c860cb85b4
Abhijeet Anand <abhijeetanand2011@gmail.com>
2025-04-11 16:34:41 -0700

    corrected table argument in return

--- qsoabsfind/spec.py
+++ qsoabsfind/spec.py
@@ -57,5 +57,5 @@ class QSOSpecRead:
             details_dict = {key: self.metadata[key] for key in self.metadata.dtype.names}
             return details_dict
         else:
-            return Table(self.metadata)
+            return return self.metadata
             
