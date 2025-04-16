17d39b0f3b3a50114b801579a6adf1fa58b83e23
a_anand <abhijeetanand2011@gmail.com>
2025-04-15 20:05:40 -0700

    autoload options added

--- qsoabsfind/spec.py
+++ qsoabsfind/spec.py
@@ -10,7 +10,7 @@ class QSOSpecRead:
     """
     A class to read and handle QSO spectra from a FITS file containing FLUX, ERROR, WAVELENGTH, and METADATA extensions."""
 
-    def __init__(self, fits_file, index=None, autoload=False, verbose=False):
+    def __init__(self, fits_file, index=None, autoload=False, verbose=True):
         """
         Initializes the QSOSpecRead class.
 
