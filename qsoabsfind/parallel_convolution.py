729f7fd6686ff488b63b99f82905eec3c04a774a
a_anand <abhijeetanand2011@gmail.com>
2025-07-09 10:38:55 -0700

    absorber choices added

--- qsoabsfind/parallel_convolution.py
+++ qsoabsfind/parallel_convolution.py
@@ -97,7 +97,7 @@ def main():
     parser = argparse.ArgumentParser(description='Parallelized convolution-based method to detect metal doublets in SDSS/DESI-like low-resolution quasar spectra using adaptive S/N.')
     parser.add_argument('--input-fits-file', type=str, required=True, help='Path to the input FITS file, containing residual spectra.')
     parser.add_argument('--n-qso', type=str, required=False, help="Number of QSO spectra to process, or a bash-like sequence (e.g., '1-1000', '1-1000:10'). If not provided, code will run all the spectra")
-    parser.add_argument('--absorber', type=str, required=True, help='Absorber name for searching doublets (MgII, CIV).')
+    parser.add_argument('--absorber', type=str, required=True, help='Absorber name for searching doublets (options: MgII, CIV, OVI, NV, SiIV, AlIII, FeII).')
     parser.add_argument('--constant-file', type=str, help='Path to the constants .py file, please follow the exact same structure as qsoabsfind.constants, i.e the default parameter that the code uses')
     parser.add_argument('--output', type=str, required=True, help='Path to the output FITS file to save absorber catalog.')
     parser.add_argument('--headers', type=str, nargs='+', help='Headers for the output FITS file in the format NAME=VALUE.')
