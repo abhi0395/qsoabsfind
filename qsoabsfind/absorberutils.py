ab14c6758c107044e65ca5d9d813e274d758a079
a_anand <abhijeetanand2011@gmail.com>
2025-07-28 14:53:48 -0700

    snr cut added

--- qsoabsfind/absorberutils.py
+++ qsoabsfind/absorberutils.py
@@ -727,7 +727,7 @@ def return_if_absorber_can_be_detected_in_a_spectrum(spectra, absorber, **kwargs
     if "snr_cut" in kwargs and kwargs["snr_cut"] is not None:
         snr_median = np.nanmedian(unmsk_residual / unmsk_error)
         print(f'INFO: Checking SNR in the wavelength search region (median SNR = {snr_median:.2f}, threshold = {kwargs["snr_cut"]})')
-        if snr_median <= kwargs["snr_cut"]:
+        if snr_median < kwargs["snr_cut"]:
             return 0
 
     return 1
