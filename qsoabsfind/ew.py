a26d6ce74433b347d45c797958bcf388257e7afc
a_anand <abhijeetanand2011@gmail.com>
2025-07-30 14:12:36 -0700

    retracted changes

--- qsoabsfind/ew.py
+++ qsoabsfind/ew.py
@@ -234,13 +234,7 @@ def measure_absorber_properties_double_gaussian(index, wavelength, flux, error, 
             sigma2 = uniform(bound[0][5], bound[1][5])
             line_second = line_centre2
             init_cond = [amp_first_nmf, line_first, sigma1, 0.54 * amp_first_nmf, line_second, sigma2]
-            # first perform fitting in observed frame
-            zf = 1 + absorber_redshift[k]
-            obs_init_cond = [amp_first_nmf, line_first * zf, sigma1 * zf, 0.54 * amp_first_nmf, line_second * zf, sigma2 * zf]
-            fitting_param_for_spectrum[k], fitting_param_std_for_spectrum[k], _,_,_,_ = double_curve_fit(
-                index, double_gaussian, lam_fit * zf, nmf_resi, error_fit=error_flux, bounds=None, init_cond=obs_init_cond, iter_n=2000)
-
-            # now in rest-frame
+            # fitting in rest-frame
             fitting_param_for_spectrum[k], fitting_param_std_for_spectrum[k], EW_first_line[k], EW_second_line[k], EW_total[k],_ = double_curve_fit(
                 index, double_gaussian, lam_fit, nmf_resi, error_fit=error_flux, bounds=bound, init_cond=init_cond, iter_n=2000)
 
@@ -261,22 +255,22 @@ def measure_absorber_properties_double_gaussian(index, wavelength, flux, error, 
             fitting_param_for_spectrum[k], fitting_param_std_for_spectrum[k], EW_first_line[k], EW_second_line[k], EW_total[k], fitting_param_pcov_for_spectrum[k] = double_curve_fit(
                 index, double_gaussian, lam_fit, nmf_resi, error_fit=error_flux, bounds=bound, init_cond=init_cond, iter_n=1000)
 
-            # fitted_l1 = fitting_param_for_spectrum[k][1]*(1+z_abs_array[k]) # in observed frame
-            # fitted_l2 = fitting_param_for_spectrum[k][4]*(1+z_abs_array[k])
-            # std_fitted_l1 = fitting_param_std_for_spectrum[k][1]*(1+z_abs_array[k])
-            # std_fitted_l2 = fitting_param_std_for_spectrum[k][4]*(1+z_abs_array[k])
+            fitted_l1 = fitting_param_for_spectrum[k][1]*(1+z_abs_array[k]) # in observed frame
+            fitted_l2 = fitting_param_for_spectrum[k][4]*(1+z_abs_array[k])
+            std_fitted_l1 = fitting_param_std_for_spectrum[k][1]*(1+z_abs_array[k])
+            std_fitted_l2 = fitting_param_std_for_spectrum[k][4]*(1+z_abs_array[k])
 
-            # z_abs_array[k], z_abs_err[k] = redshift_estimate(fitted_l1, fitted_l2, std_fitted_l1, std_fitted_l2, line_centre1, line_centre2)
+            z_abs_array[k], z_abs_err[k] = redshift_estimate(fitted_l1, fitted_l2, std_fitted_l1, std_fitted_l2, line_centre1, line_centre2)
 
-            # #best-fit corresponding to this best redshift
-            # absorber_rest_lam = wavelength / (1 + z_abs_array[k]) # rest-frame conversion of wavelength
-            # lam_ind = np.where((absorber_rest_lam >= ix0) & (absorber_rest_lam <= ix1))[0]
-            # lam_fit = absorber_rest_lam[lam_ind]
-            # nmf_resi = flux[lam_ind]
-            # error_flux = error[lam_ind]
+            #best-fit corresponding to this best redshift
+            absorber_rest_lam = wavelength / (1 + z_abs_array[k]) # rest-frame conversion of wavelength
+            lam_ind = np.where((absorber_rest_lam >= ix0) & (absorber_rest_lam <= ix1))[0]
+            lam_fit = absorber_rest_lam[lam_ind]
+            nmf_resi = flux[lam_ind]
+            error_flux = error[lam_ind]
 
-            # fitting_param_for_spectrum[k], fitting_param_std_for_spectrum[k], EW_first_line[k], EW_second_line[k], EW_total[k], fitting_param_pcov_for_spectrum[k] = double_curve_fit(
-            #     index, double_gaussian, lam_fit, nmf_resi, error_fit=error_flux, bounds=bound, init_cond=init_cond, iter_n=1000)
+            fitting_param_for_spectrum[k], fitting_param_std_for_spectrum[k], EW_first_line[k], EW_second_line[k], EW_total[k], fitting_param_pcov_for_spectrum[k] = double_curve_fit(
+                index, double_gaussian, lam_fit, nmf_resi, error_fit=error_flux, bounds=bound, init_cond=init_cond, iter_n=1000)
 
             ## errors on EW
             if not use_covariance:
